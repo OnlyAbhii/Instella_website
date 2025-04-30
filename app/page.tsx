832893a68d458e09fc4514653ebfe9be2de5e5b6
OnlyAbhii <chandilaayush5@gmail.com>
2025-04-30 23:50:14 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -155,7 +155,7 @@ export default function Home() {
           initial={{ opacity: 0, y: -10 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{
-            duration: 1,
+            duration: 1.5,
             delay: 1.5,
             repeat: Number.POSITIVE_INFINITY,
             repeatType: "loop",
